935e7ce358e2dd3664bb5012084129694c2a0e06
Эмиль Искандеров <emilagamamedov@gmail.com>
2021-07-11 18:03:33 +0300

    Добавил url для статических файлов

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.schemas import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
     ), name='openapi-schema'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
